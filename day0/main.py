27d2a45d5b9298ca56e06a6ce042d3bdc9506c05
Owen-Griffin <99355709+Owen-Griffin@users.noreply.github.com>
2023-07-07 17:43:15 -0400

    Update main.py

--- day0/main.py
+++ day0/main.py
@@ -1,6 +1,8 @@
-def main():
-  print('Hello, World.')
-  print(input())
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
 
-if __name__ == '__main__':
-  main()
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+print(input_string)
