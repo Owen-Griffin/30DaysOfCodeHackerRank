e1d92db0b33ec22042198c43fc40aa5744d2f2bd
Owen-Griffin <99355709+Owen-Griffin@users.noreply.github.com>
2023-07-08 12:19:48 -0400

    Create main.py

---
+++ day1/main.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+intInput = int(input())
+floatInput = float(input())
+strInput = str(input())
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i + intInput)
+print(d + floatInput)
+print(s + strInput)
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
